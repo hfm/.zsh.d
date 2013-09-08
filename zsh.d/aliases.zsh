0ca23249c5777181ffc367899401069061c7abd2
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-09-08 23:58:04 +0900

    エイリアスを修正した

--- zsh.d/aliases.zsh
+++ zsh.d/aliases.zsh
@@ -1,78 +1,60 @@
-#  ____ ____  ____
-# | __ ) ___||  _ \
-# |  _ \___ \| | | |
-# | |_) |__) | |_| |
-# |____/____/|____/
-
-alias cp="cp -i"
-alias du="du -h"
-alias df="df -h"
-alias grep="grep --color"
-alias ll="ls -hal"
-alias mv="mv -i -v"
-alias rl="/usr/local/bin/r"
-alias rm="rm -i"
-alias vi="vim"
-alias gitignoring="~/.junk/gitignore_reloaded.sh"
+alias be='bundle exec'
+alias cp='cp -i'
+alias du='du -h'
+alias df='df -h'
+alias grep='grep --color'
+alias ll='ls -hal'
+alias mv='mv -i -v'
+alias rm='rm -i'
+alias ssh='TERM=xterm ssh'
+alias vi='vim'
 alias -s {txt,md,markdown,xml}=lv
-alias -s py=python
-alias -s hs=runhaskell
-alias be="bundle exec"
 
-#   ___  ____   __  __
-#  / _ \/ ___|  \ \/ /
-# | | | \___ \   \  /
-# | |_| |___) |  /  \
-#  \___/|____/  /_/\_\
+#                .-')   ) (`-.      
+#               ( OO ).  ( OO ).    
+#  .-'),-----. (_)---\_)(_/.  \_)-. 
+# ( OO'  .-.  '/    _ |  \  `.'  /  
+# /   |  | |  |\  :` `.   \     /\  
+# \_) |  |\|  | '..`''.)   \   \ |  
+#   \ |  | |  |.-._)   \  .'    \_) 
+#    `'  '-'  '\       / /  .'.  \  
+#      `-----'  `-----' '--'   '--' 
+if [ `uname -s` = 'Darwin' ]; then
+  alias bu='brew update'
+  alias bi='brew info'
+  alias br='brew upgrade'
+  alias bs='brew -S'
 
-if [ `uname -s` = "Darwin" ]; then
-# Homebrew
-alias bu="brew update"
-alias br="brew upgrade"
-alias bs="brew -S"
-alias bi="brew info"
+  alias preview='open -a Preview'
+  alias -s {gif,png,jpg,bmp,pdf,GIF,PNG,JPG,BMP,PDF}=preview
 
-# Preview
-alias preview='open -a Preview'
-alias -s {gif,png,jpg,bmp,pdf,GIF,PNG,JPG,BMP,PDF}=preview
+  alias chrome='open -a Google\ Chrome'
+  alias -s html=chrome
 
-# Chrome
-alias google-chrome='open -a Google\ Chrome'
-alias chrome='google-chrome'
-alias -s html=chrome
-alias git=hub
-alias gitPullReq='git pull-request'
-alias weechat-curses='TERM=screen-256color weechat-curses'
+  alias git=hub
+  alias gitPullReq='git pull-request'
+  alias weechat-curses='TERM=screen-256color weechat-curses'
 
-function extract() {
-  case $1 in
-    *.tar.gz|*.tgz) tar xzvf $1;;
-  *.tar.xz) tar Jxvf $1;;
-  *.zip) unzip $1;;
-  *.lzh) lha e $1;;
-  *.tar.bz2|*.tbz) tar xjvf $1;;
-  *.tar.Z) tar zxvf $1;;
-  *.gz) gzip -dc $1;;
-  *.bz2) bzip2 -dc $1;;
-  *.Z) uncompress $1;;
-  *.tar) tar xvf $1;;
-  *.arj) unarj $1;;
-  esac
-}
+  function extract() {
+    case $1 in
+      *.tar.gz|*.tgz) tar xzvf $1;;
+      *.tar.xz) tar Jxvf $1;;
+      *.zip) unzip $1;;
+      *.lzh) lha e $1;;
+      *.tar.bz2|*.tbz) tar xjvf $1;;
+      *.tar.Z) tar zxvf $1;;
+      *.gz) gzip -dc $1;;
+      *.bz2) bzip2 -dc $1;;
+      *.Z) uncompress $1;;
+      *.tar) tar xvf $1;;
+      *.arj) unarj $1;;
+    esac
+  }
 
-alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
-function runcpp () { g++ -O2 $1; ./a.out }
-alias -s {c,cpp}=runcpp
+  alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
+  function runcpp () {
+    g++ -O2 $1;
+    ./a.out
+  }
+  alias -s {c,cpp}=runcpp
 fi
-
-
-#  _     _                  
-# | |   (_)_ __  _   ___  __
-# | |   | | '_ \| | | \ \/ /
-# | |___| | | | | |_| |>  < 
-# |_____|_|_| |_|\__,_/_/\_\
-
-if [ `uname -s` = "Linux" ]; then
-
-fi
-
