2e448675e5818b8e31b9279f6f0b828d7a73e3d0
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-09-24 04:31:56 +0900

    ちょっとわかりやすくした

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
-echo linking...
-ln -sf $HOME/.dotfiles/myzsh/zlogin $HOME/.zlogin
-ln -sf $HOME/.dotfiles/myzsh/zshenv $HOME/.zshenv
-ln -sf $HOME/.dotfiles/myzsh/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/myzsh/zsh.d $HOME/.zsh.d
-echo done.
+cat <<'EOF'
+
+,--.,--.        ,--.    ,--.                            
+|  |`--',--,--, |  |,-. `--',--,--,  ,---.              
+|  |,--.|      \|     / ,--.|      \| .-. |             
+|  ||  ||  ||  ||  \  \ |  ||  ||  |' '-' '.--..--..--. 
+`--'`--'`--''--'`--'`--'`--'`--''--'.`-  / '--''--''--' 
+                                    `---'               
+
+EOF
+
+ln -sF $HOME/.dotfiles/myzsh/zlogin $HOME/.zlogin
+ln -sF $HOME/.dotfiles/myzsh/zshenv $HOME/.zshenv
+ln -sF $HOME/.dotfiles/myzsh/zshrc  $HOME/.zshrc
+ln -sF $HOME/.dotfiles/myzsh/zsh.d  $HOME/.zsh.d
+ls -hal ~/.zsh{.d,env,rc} ~/.zlogin
+
+cat <<'EOF'
+
+   ,--.                       
+ ,-|  | ,---. ,--,--,  ,---.  
+' .-. || .-. ||      \| .-. : 
+\ `-' |' '-' '|  ||  |\   --. 
+ `---'  `---' `--''--' `----' 
+
+EOF
+
