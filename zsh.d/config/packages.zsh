50795de5a0d560263d3bc006bdecd7cd087d6f0e
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-09-24 04:33:11 +0900

    auto-fu復活

--- zsh.d/config/packages.zsh
+++ zsh.d/config/packages.zsh
@@ -25,6 +25,13 @@ bindkey '^R' zaw-history
 # zsh-completions
 package-install github zsh-users/zsh-completions
 
+# auto-fu
+package-install github hchbaw/auto-fu.zsh
+source $(package-directory hchbaw/auto-fu.zsh)/auto-fu.zsh
+zle-line-init () {auto-fu-init;}; zle -N zle-line-init
+zstyle ':completion:*' completer _oldlist _complete
+zle -N zle-keymap-select auto-fu-zle-keymap-select
+
 # zsh-completions
-package-install github zsh-users/zsh-syntax-highlighting
-source $(package-directory zsh-users/zsh-syntax-highlighting)/zsh-syntax-highlighting.zsh
+# package-install github zsh-users/zsh-syntax-highlighting
+# source $(package-directory zsh-users/zsh-syntax-highlighting)/zsh-syntax-highlighting.zsh
