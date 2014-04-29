f73be75754c6c1d1a5cd8459eb8b34e20592a0ac
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-30 06:37:38 +0900

    use percol instead of zaw

--- config/packages.zsh
+++ config/packages.zsh
@@ -2,26 +2,6 @@
 
 source ~/.zsh.d/package.zsh
 
-
-# zaw
-package-install github zsh-users/zaw
-
-autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-add-zsh-hook chpwd chpwd_recent_dirs
-zstyle ':chpwd:*' recent-dirs-max 5000
-zstyle ':chpwd:*' recent-dirs-default yes
-zstyle ':completion:*' recent-dirs-insert both
-
-source $(package-directory zsh-users/zaw)/zaw.zsh
-
-zstyle ':filter-select' case-insensitive yes
-zstyle ':filter-select' max-lines 10
-zstyle ':filter-select' max-lines -10
-zstyle ':filter-select:highlight' matched fg=yellow,standout
-
-bindkey '^@' zaw-cdr
-# bindkey '^R' zaw-history
-
 # zsh-completions
 package-install github zsh-users/zsh-completions
 
@@ -31,7 +11,6 @@ source $(package-directory hchbaw/auto-fu.zsh)/auto-fu.zsh
 zle-line-init () {auto-fu-init;}; zle -N zle-line-init
 zstyle ':completion:*' completer _oldlist _complete
 zle -N zle-keymap-select auto-fu-zle-keymap-select
-
 
 # package-install github zsh-users/zsh-syntax-highlighting
 # source $(package-directory zsh-users/zsh-syntax-highlighting)/zsh-syntax-highlighting.zsh
--- percol.zsh
+++ percol.zsh
@@ -1,3 +1,4 @@
+# read function.zsh beforehand
 if exists percol; then
     function percol_select_history() {
         local tac
@@ -21,5 +22,27 @@ if exists percol; then
 
     function ppkill() {
         ps ax | percol | awk '{ print $1 }' | xargs kill
+    }
+
+    ### http://piyopiyoducky.net/blog/2013/08/17/cdr-with-percol/
+    ### search a destination from cdr list
+    function percol-get-destination-from-cdr() {
+        cdr -l | \
+            sed -e 's/^[[:digit:]]*[[:blank:]]*//' | \
+            percol --match-method migemo --query "$LBUFFER"
+    }
+
+    ### search a destination from cdr list and cd the destination
+    function percol-cdr() {
+        local destination="$(percol-get-destination-from-cdr)"
+        if [ -n "$destination" ]; then
+            BUFFER="cd $destination"
+            zle accept-line
+        else
+            zle reset-prompt
+        fi
+
+    zle -N percol-cdr
+    bindkey '^@' percol-cdr
     }
 fi
