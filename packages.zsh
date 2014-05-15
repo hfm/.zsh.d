87cc7ff4a118500ec8db7d3b2c2527d3c9e3c832
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-05-15 13:16:43 +0900

    add zsh-bd

--- packages.zsh
+++ packages.zsh
@@ -9,3 +9,12 @@ source $HOME/.zsh.d/packages/auto-fu.zsh/auto-fu.zsh
 zle-line-init () {auto-fu-init;}; zle -N zle-line-init
 zstyle ':completion:*' completer _oldlist _complete
 zle -N zle-keymap-select auto-fu-zle-keymap-select
+
+# ===============================
+#  ____ ____ ____ ____ ____ ____ 
+# ||z |||s |||h |||- |||b |||d ||
+# ||__|||__|||__|||__|||__|||__||
+# |/__\|/__\|/__\|/__\|/__\|/__\|
+#
+# ===============================
+source $HOME/.zsh.d/packages/zsh-bd/bd.zsh
