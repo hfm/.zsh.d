ebba52834c25d2763f016737704a30e73aa3cd95
Okumura Takahiro <hfm@cookpad.com>
2019-02-06 17:24:20 +0900

    skips

--- 30packages.zsh
+++ 30packages.zsh
@@ -8,6 +8,9 @@
 source $ZDOTDIR/packages/auto-fu.zsh/auto-fu.zsh
 zle-line-init () {auto-fu-init;}; zle -N zle-line-init
 zstyle ':completion:*' completer _oldlist _complete
+
+zstyle ':auto-fu:var' postdisplay ''
+zstyle ':auto-fu:var' autoable-function/skiplines '*rake *' 'gem *' 'git log *' 'git commit *' 'scp *'  'svn commit *' 'cap *'
 zle -N zle-keymap-select auto-fu-zle-keymap-select
 
 # ========================================================
